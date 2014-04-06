f2806f4983e2fcae2d97dd837c8987eb473ff872
James MacAulay <james@shopify.com>
2014-04-06 16:48:20 -0400

    project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,13 @@
 (defproject cljs-promises "0.1.0-SNAPSHOT"
-  :description "A ClojureScript library for using JS promises with core.async"
+  :description "A ClojureScript library for working with JavaScript promises"
+
   :url "https://github.com/jamesmacaulay/cljs-promises"
+
+  :scm {:name "git"
+        :url "https://github.com/jamesmacaulay/cljs-promises"}
+
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojurescript "0.0-2156"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
